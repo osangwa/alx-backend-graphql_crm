f0868ad0605e5011791ba41fff31042a2f9aac9d
osangwa <osangwa@environment.gov.rw>
2025-10-19 23:11:04 +0200

    fix

--- graphql_crm/schema.py
+++
@@ -1,9 +0,0 @@
-import graphene
-from crm.schema import Query as CRMQuery
-
-class Query(CRMQuery, graphene.ObjectType):
-    # This combines queries from different apps
-    pass
-
-# THIS LINE IS CRITICAL FOR THE CHECKS
-schema = graphene.Schema(query=Query)

ade6cd013a2e65b6b7ee42245a20dd414f0a40cf
osangwa <osangwa@environment.gov.rw>
2025-10-19 23:27:31 +0200

    fix3-uat

--- crm/schema.py
+++ crm/schema.py
@@ -1,5 +1,36 @@
 import graphene
+from graphene_django import DjangoObjectType
+from .models import Customer, Product, Order
+
+class CustomerType(DjangoObjectType):
+    class Meta:
+        model = Customer
+        fields = ("id", "name", "email", "phone")
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+class OrderType(DjangoObjectType):
+    class Meta:
+        model = Order
+        fields = "__all__"
+
 class Query(graphene.ObjectType):
     hello = graphene.String()
+    all_customers = graphene.List(CustomerType)
+    all_products = graphene.List(ProductType)
+    all_orders = graphene.List(OrderType)
+
     def resolve_hello(self, info):
         return "Hello, GraphQL!"
+
+    def resolve_all_customers(self, info):
+        return Customer.objects.all()
+
+    def resolve_all_products(self, info):
+        return Product.objects.all()
+
+    def resolve_all_orders(self, info):
+        return Order.objects.all()

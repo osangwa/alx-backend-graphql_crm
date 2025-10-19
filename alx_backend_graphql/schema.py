a35806cf7352fb32d6ae52db52ed99f580cec93a
osangwa <osangwa@environment.gov.rw>
2025-10-19 23:19:31 +0200

    fix number 2

---
+++ alx_backend_graphql/schema.py
@@ -0,0 +1,5 @@
+import graphene
+from crm.schema import Query as CRMQuery
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+schema = graphene.Schema(query=Query)
--- crm/schema.py
+++ crm/schema.py
@@ -1,8 +1,5 @@
 import graphene
-
 class Query(graphene.ObjectType):
-    # This class must inherit from graphene.ObjectType
     hello = graphene.String()
-
     def resolve_hello(self, info):
         return "Hello, GraphQL!"
